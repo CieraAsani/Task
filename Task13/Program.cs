e9a095bd80bef7903ce88b9b234b1a9bcc567389
Furde <furde123@gmail.co>
2023-04-28 21:57:52 +0300

    added Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+int SecondNumber =Number.ToString().Length;
+if (SecondNumber >= 3) 
+{
+	while (Number > 999)
+	{
+		Number = Number/ 10;
+	}
+	int result = Number % 10;
+	Console.WriteLine("Третья цифра числа -> " + result);
+} else {
+	Console.WriteLine("Третьей цифры нету,дружок)");
+}
